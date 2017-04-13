1bd6295a4d9149342407bb2f85bb1c9038adc29d
Dmitry Miroshnichenko <2210605@gmail.com>
2017-04-14 00:11:10 +0300

    Implement UserTweets methods

--- code/004/usertweets.py
+++ code/004/usertweets.py
@@ -15,14 +15,18 @@ Tweet = namedtuple('Tweet', 'id_str created_at text')
 
 
 class UserTweets(object):
-
     def __init__(self, handle, max_id=None):
         """Get handle and optional max_id.
         Use tweepy.OAuthHandler, set_access_token and tweepy.API
         to create api interface.
         Use _get_tweets() helper to get a list of tweets.
         Save the tweets as data/<handle>.csv"""
-        # ...
+
+        self.auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+        self.auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+        self.api = tweepy.API(self.auth)
+
+        self.handle = handle
         self._tweets = list(self._get_tweets())
         self._save_tweets()
 
@@ -31,18 +35,24 @@ class UserTweets(object):
         See tweepy API reference: http://docs.tweepy.org/en/v3.5.0/api.html
         Use a list comprehension / generator to filter out fields
         id_str created_at text (optionally use namedtuple)"""
-        pass
+        raw_tweets = [status for status in tweepy.Cursor(self.api.user_timeline, self.handle).items(100)]
+        return [Tweet(tweet.id_str, tweet.created_at, tweet.text) for tweet in raw_tweets]
 
     def _save_tweets(self):
         """Use the csv module (csv.writer) to write out the tweets.
         If you use a namedtuple get the column names with Tweet._fields.
         Otherwise define them as: id_str created_at text
         You can use writerow for the header, writerows for the rows"""
-        pass
+        with open(f'./{DEST_DIR}/test.{EXT}', 'a') as f:
+            writer = csv.writer(f)
+            writer.writerow(('id_str', 'created_at', 'text'))
+            [writer.writerow(triple) for triple in self._tweets]
+
 
     def __len__(self):
         """See http://pybit.es/python-data-model.html"""
         pass
+
 
     def __getitem__(self, pos):
         """See http://pybit.es/python-data-model.html"""
@@ -54,6 +64,6 @@ if __name__ == "__main__":
     for handle in ('pybites', 'techmoneykids', 'bbelderbos'):
         print('--- {} ---'.format(handle))
         user = UserTweets(handle)
-        for tw in user[:5]:
-            print(tw)
-        print()
+        # for tw in user[:5]:
+        #     print(tw)
+        # print()
