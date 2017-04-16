e041f0cf3767c3f0a71e73f74a7dea92867ebd77
Dmitry Miroshnichenko <2210605@gmail.com>
2017-04-16 23:46:49 +0300

    Debug

--- code/005/similar_tweeters.py
+++ code/005/similar_tweeters.py
@@ -1,7 +1,10 @@
 import sys
-from usertweets import UserTweets
+import re
+import itertools
 import nltk, string
 from sklearn.feature_extraction.text import TfidfVectorizer
+
+from usertweets import UserTweets
 
 def similar_tweeters(user1, user2):
     userTweets1 = UserTweets(user1)
@@ -22,15 +25,33 @@ def similar_tweeters(user1, user2):
 
     vectorizer = TfidfVectorizer(tokenizer=normalize, stop_words='english')
 
+
     def cosine_sim(text1, text2):
         tfidf = vectorizer.fit_transform([text1, text2])
         return ((tfidf * tfidf.T).A)[0, 1]
 
 
-    for idx in range(90):
-        score = cosine_sim(userTweets1[idx].text, userTweets2[idx].text)
-        if score > 0:
-            print(f'Score was {score}\nUserText1 is {userTweets1[idx].text}\nUserText2 is {userTweets2[idx].text}')
+    for tweet1 in userTweets1._tweets:
+        for tweet2 in userTweets2._tweets:
+
+            clean_text1 = ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)|(RT|Python)", " ", tweet1.text).split())
+            clean_text2 = ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)|(RT|Python)", " ", tweet2.text).split())
+
+            if len(clean_text1.split()) and len(clean_text2.split()) > 3:
+                try:
+                    score = cosine_sim(clean_text1, clean_text2)
+                except ValueError as err:
+                    print(err)
+                    print(f'Before {tweet1.text}, after {clean_text1}')
+                    print(f'Before {tweet2.text}, after {clean_text2}')
+                    continue
+
+                if score > 0.5:
+                    print(f'Score was {score}\nUserText1 is {clean_text1}\nUserText2 is {clean_text2}')
+
+
+    # for i in range(1, 90):
+    #     yield from list(itertools.permutations(draw, i))
 
 if __name__ == "__main__":
     # if len(sys.argv) < 3:
