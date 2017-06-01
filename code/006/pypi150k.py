98c0433f8ddfdbac98922f5d861c21a75e6f2088
Dmitriy Miroshnichenko <dmiroshnichenko@ptsecurity.com>
2017-06-01 20:44:02 +0300

    Add links to useful articles

--- code/006/pypi150k.py
+++ code/006/pypi150k.py
@@ -1,3 +1,9 @@
+# https://www.digitalocean.com/community/tutorials/a-guide-to-time-series-forecasting-with-arima-in-python-3
+# https://www.analyticsvidhya.com/blog/2016/02/time-series-forecasting-codes-python/
+# https://www.digitalocean.com/community/tutorials/a-guide-to-time-series-forecasting-with-prophet-in-python-3
+# http://machinelearningmastery.com/time-series-forecasting-python-mini-course/
+
+import pickle
 import requests
 import xmlrpc.client as xmlrpclib
 from dateutil import parser
@@ -11,25 +17,30 @@ packages = client.list_packages()
 
 packages_time = {}
 
+
 def fetch_date(package_name):
     url = f'{PYPI}/{package_name}/json'
     r = requests.get(url)
     # СПИСОК УСПЕШНЫХ КОДОВ !!!
-    #if r.status_code == requests.codes.ok:
+    # if r.status_code == requests.codes.ok:
     if r.status_code == 200:
         if len(r.json()['urls']) > 0:
             time = parser.parse(r.json()['urls'][-1]['upload_time'])
             packages_time[time] = package_name
+            packages.remove(package_name)
         else:
             packages_time[None] = package_name
+            packages.remove(package_name)
     else:
         pass
+
 
 pool = ThreadPool(10)
 pool.map(fetch_date, packages)
 pool.close()
 pool.join()
 
+pickle.dump(packages_time, open("save.p", "wb"))
 
 if __name__ == "__main__":
     pass
