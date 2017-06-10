63adf978e3f21f6ce9beb5d0954d88aeb08e2138
Dmitry Miroshnichenko <2210605@gmail.com>
2017-06-10 22:30:31 +0300

    Add House class Add menu

--- code/008/inventory.py
+++ code/008/inventory.py
@@ -1,3 +1,36 @@
+"""
+Create some sort of program shell with a menu system around this. ie, "Which room would you like to list out?"
+
+If you're really game, allow users to create rooms and update information.
+
+You could even make an API with Flask or your preferred framework
+
+Have persistent storage of the data. sqlite3 = stdlib and light-weight, but feel free to use your preferred DB / module.
+"""
+
+
+class House:
+    rooms = []
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def add_room(room):
+        House.rooms.append(room)
+
+    @staticmethod
+    def list_rooms():
+        for room in House.rooms:
+            print(f"\n{room.name.upper():<8}")
+            print(f"{'ITEM':<8} {'COST':<8}")
+            for item in room.items.items():
+                print("{:<8} {:<8}".format(*item))
+
+        money = [list(room.items.values()) for room in House.rooms]
+        print(f"\nTOTAL: {sum([y for x in money for y in x]):>5}")
+
+
 class Room:
     """
     This class define room
@@ -27,12 +60,40 @@ class Room:
     def list_items(self):
         return len(self.items)
 
-def list_rooms(rooms):
-    for room in rooms:
-        print(f"\n{room.name.upper():<8}")
-        print(f"{'ITEM':<8} {'COST':<8}")
-        for item in room.items.items():
-            print("{:<8} {:<8}".format(*item))
 
-    money = [list(room.items.values()) for room in rooms]
-    print(f"\nTOTAL: {sum([y for x in money for y in x]):>5}")
+def menu():
+    print("""
+    1). Show available rooms          +--+
+    2). Add new room                  |  |
+    3). Add item to the room          |  |
+    4). Remove item from the room     |  |  XXXXXXXXXXXXX
+    5). Exit                          |  |XXX           XXX
+                                      |  |X               XXX
+                                      +--+                  XX
+                                     XXX                     XXX
+                                    XX------------------------XXX
+                                    +                           |
+                                    |                           |
+                                    |  +------+      +------+   +-----+
+                                    |  |___|__|      |___|__|   |     |
+                                    |  |   |  |      |   |  |   |     |
+                                    |  +------+      +------+   |    ++
+                                    |                           |    ++
+                                    |                           |     |
+                                    |                           |     |
+                                    +---------------------------+-----+
+    """)
+
+def main():
+    house = House()
+    house.add_room(Room(name='bedroom', items={'cup': 3, 'bath': 200, 'soap': 1, 'item4': 12, 'item5': 33}))
+    house.add_room(Room(name='bathroom', items={'cup': 4, 'bath': 100, 'soap': 6, 'item4': 52, 'item5': 23}))
+    house.add_room(Room(name='kitchen', items={'cup': 5, 'bath': 20, 'soap': 5, 'item4': 4, 'item5': 53}))
+    house.add_room(Room(name='baby room', items={'cup': 6, 'bath': 500, 'soap': 2, 'item4': 32, 'item5': 63}))
+    house.add_room(Room(name='living room', items={'cup': 73, 'bath': 50, 'soap': 3, 'item4': 22, 'item5': 73}))
+
+    house.list_rooms()
+
+
+if __name__ == '__main__':
+    main()
