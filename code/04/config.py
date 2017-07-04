cc8aec3777ad3fd89d30077ef729a7aa743f5ba9
Dmitry Miroshnichenko <2210605@gmail.com>
2017-07-04 23:58:08 +0300

    add new challenges

---
+++ code/04/config.py
@@ -0,0 +1,5 @@
+# Getting your API keys from Twitter - https://apps.twitter.com
+CONSUMER_KEY = 'jreHSjor3cTDPMWR6CDqmyhgx'
+CONSUMER_SECRET = 'mZBqtXj1Q1sf3DaaFKqRniCmGWBp9qw6nwFoVrLR5koqljFc7G'
+ACCESS_TOKEN = '24383889-VhJqtVUmgSdufqSb1z10hmgN0cepZqXf5Ksy7oqPT'
+ACCESS_SECRET = 'rCe2ughe0bwODXgm5FYgJZ03fYydkyWtjmmqDSYPKvUIq'
