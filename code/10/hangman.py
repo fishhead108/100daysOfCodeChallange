e5fec1c696823044a2cdfd6db6cc11ac064487b7
Dmitry Miroshnichenko <2210605@gmail.com>
2017-07-06 23:52:22 +0300

    My answer

--- code/10/hangman.py
+++ code/10/hangman.py
@@ -1,19 +1,54 @@
 from string import ascii_lowercase
 import sys
 
-from movies import get_movie as get_word  # keep interface generic
+from movies import get_movie as get_word
 from graphics import hang_graphics
 
 ASCII = list(ascii_lowercase)
-HANG_GRAPHICS = list(hang_graphics())
-ALLOWED_GUESSES = len(HANG_GRAPHICS)
-PLACEHOLDER = '_'
+HANG_GRAPHICS = hang_graphics()
 
 
-class Hangman(object):
-    pass 
+def main(word):
+    def check_input_char(char):
+        if char in present_word:
+            print("You already know this character")
+            return True
 
-# or use functions ...
+        if char in finish_word:
+            for index, character in enumerate(finish_word):
+
+                if character == char:
+                    present_word[index] = character
+
+                    if present_word == finish_word:
+                        print(f"You WIN !\nAnswer is: {secret_word}")
+                        exit(0)
+
+            return True
+
+    secret_word = word
+    finish_word = ([char for char in secret_word])
+    present_word = (['_' for char in secret_word])
+
+    while True:
+
+        print("".join(present_word))
+        char = input("Enter the letter: ")
+
+        if char in ASCII:
+
+            if check_input_char(char):
+                pass
+
+            else:
+                try:
+                    print(next(HANG_GRAPHICS))
+                except StopIteration:
+                    print("YOU ARE DEAD")
+                    exit(1)
+
+        else:
+            print("Wrong character, type another")
 
 
 if __name__ == '__main__':
@@ -21,6 +56,4 @@ if __name__ == '__main__':
         word = sys.argv[1]
     else:
         word = get_word()
-    print(word)
-
-    # init / call program
+    main(word)
