501ad65112364d14a63e700323972f2f323a51d8
Dmitry Miroshnichenko <2210605@gmail.com>
2017-07-07 00:06:28 +0300

    Fix the ASCCI check

--- code/10/hangman.py
+++ code/10/hangman.py
@@ -9,6 +9,7 @@ HANG_GRAPHICS = hang_graphics()
 
 
 def main(word):
+
     def check_input_char(char):
         if char in present_word:
             print("You already know this character")
@@ -28,32 +29,28 @@ def main(word):
 
     secret_word = word
     finish_word = ([char for char in secret_word])
-    present_word = (['_' for char in secret_word])
+    present_word = (['_' if c in ASCII else c for c in secret_word])
 
     while True:
 
         print("".join(present_word))
-        char = input("Enter the letter: ")
+        char = (input("Enter the letter: ")).lower()
 
-        if char in ASCII:
-
-            if check_input_char(char):
-                pass
-
-            else:
-                try:
-                    print(next(HANG_GRAPHICS))
-                except StopIteration:
-                    print("YOU ARE DEAD")
-                    exit(1)
+        if check_input_char(char):
+            pass
 
         else:
-            print("Wrong character, type another")
+            try:
+                print(next(HANG_GRAPHICS))
+            except StopIteration:
+                print("YOU ARE DEAD")
+                exit(1)
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         word = sys.argv[1]
+        word = word.lower()
     else:
         word = get_word()
     main(word)
